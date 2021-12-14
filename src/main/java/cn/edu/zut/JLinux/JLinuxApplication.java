2d5bffed0be8557dc1c7853a435308eade6d9212
daofaziran1 <liyongzhuang@outlook.com>
2021-12-14 16:04:54 +0800

    init

---
+++ src/main/java/cn/edu/zut/JLinux/JLinuxApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.zut.JLinux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JLinuxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JLinuxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/zut/JLinux/JLinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zut.JLinux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JLinuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
